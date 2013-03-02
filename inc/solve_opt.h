97aa31140a583620cabe8860558ee0863b1f6772
Chunyang Xiao <chunyang.xiao@student.ecp.fr>
2013-03-02 16:14:21 +0100

    comments

--- inc/solve_opt.h
+++ inc/solve_opt.h
@@ -8,19 +8,25 @@
 
 // R,G,B independent
 
-// Solve energy optimization problem and render image
+/*
+  For the solution of gradient matrix problem, we use alglib solvers. 
+  We use alglib::sparsematrix to solve the linear problem, the later, by using conjugate gradient algorithm, this allows us to solve problem in O(n^2)
+  N the number of pixels in the generating image. 
+*/
+
+// Solve energy optimization problem and render image, O(n), n the number of pixels in synthesized image
 void solve_basic(const std::vector<cv::Point>& z, const std::vector<cv::Point>& x, const cv::Mat& ref_image, cv::Mat& render_image, int width);
 
-// Solve energy optimization problem with gaussian weights
+// Solve energy optimization problem with gaussian weights, O(n), n the number of pixels in synthesized image
 void solve_gaussian(const std::vector<cv::Point>& z, const std::vector<cv::Point>& x, const cv::Mat& ref_image, cv::Mat& render_image, int width);
 
-// Solve energy optimization problem with gaussian weights and different patch weights 
+// Solve energy optimization problem with gaussian weights and different patch weights, n the number of pixels in synthesized image 
 void wsolve_gaussian(const std::vector<cv::Point>& z, const std::vector<cv::Point>& x, const cv::Mat& ref_image, cv::Mat& render_image, int width, const std::vector<double>& weights);
 
-// Solve energy optimization problem with gradient in energy function 
+// Solve energy optimization problem with gradient in energy function, O(n^2), n the number of pixels in synthesized image 
 void solve_grad(const std::vector<cv::Point>& z, const std::vector<cv::Point>& x, const cv::Mat& ref_image, cv::Mat& render_image, int width);
 
-// Solve energy optimization problem with gradient in energy function and with gaussia weights
+// Solve energy optimization problem with gradient in energy function and with gaussian weights, O(n^2), n the number of pixels in synthesized image 
 void solve_ggrad(const std::vector<cv::Point>& z, const std::vector<cv::Point>& x, const cv::Mat& ref_image, cv::Mat& render_image, int width);
 
 
--- src/solve_opt.cpp
+++ src/solve_opt.cpp
@@ -35,6 +35,7 @@ void print_array(const real_1d_array& b, int length)
   cout << endl; 
 }
 
+// Initialization of array in alglib
 void init_2d_array(real_2d_array& A, int nrow, int ncol)
 {
   for(int i = 0; i < nrow; ++i)
@@ -85,6 +86,14 @@ void solve_one_channel_gaussian(const vector<Point>& z, const vector<Point>& x, 
     }
   }
 }
+
+/*
+  Solve energy optimization problem and with gaussian weights render image, O(n), n the number of pixels in synthesized image.
+  As the energy is quadratic in patch, E = |px-pz|^2
+  For each patch, we apply a gaussian fall-off function with gaussian center the patch center, then, by giving the gradient expression equal to zero, we obtain:
+    X[x,y] = (sum(gaussian(i)))*[gaussian(1)Z1+gaussian(2)Z2+...+gaussian(n)Zn], n the nearest neighbours of neighbour that contains pixels X[x,y], with gaussian_weight = exp(-(x_NH*x_NH+y_NH*y_NH)/(2*gamma*gamma)), x_NH, y_NH the distance to the center of the patch
+  We could see that the gradient do not have crosses terms, so we calculate the above sum directly which gives O(n) complexity.   
+*/
 
 void solve_gaussian(const vector<Point>& z, const vector<Point>& x, const Mat& ref_image, Mat& image, int width)
 {
@@ -151,6 +160,13 @@ void wsolve_one_channel_gaussian(const vector<Point>& z, const vector<Point>& x,
     }
   }
 }
+
+/*
+  Solve energy optimization problem and with gaussian weights and robust optimization and then render image, O(n), n the number of pixels in synthesized image.
+  For each patch, we apply a gaussian fall-off function with gaussian center the patch center and here for a given patch, the function wsolve_gaussian implements an robust optimization process,  the energy is quadratic in patch becomes E = |px-pz|^0.8 instead of being quadratic. By giving the gradient expression equal to zero, we obtain:
+    X[x,y] = (sum(gaussian(i)))*[gaussian(1)Z1+gaussian(2)Z2+...+gaussian(n)Zn], n the nearest neighbours of neighbour that contains pixels X[x,y], with gaussian_weight = exp(-(x_NH*x_NH+y_NH*y_NH)/(2*gamma*gamma)), x_NH, y_NH the distance to the center of the patch
+  We could see that the gradient do not have crosses terms, so we calculate the above sum directly which gives O(n) complexity.
+*/
 
 void wsolve_gaussian(const vector<Point>& z, const vector<Point>& x, const Mat& ref_image, Mat& image, int width, const vector<double>& weights)
 {
@@ -277,6 +293,27 @@ void solve_one_channel_grad(const vector<Point>& z, const vector<Point>& x, cons
   }   
 }
 
+/*
+  Solve energy optimization problem and with gaussian weights render image, O(n^2), n the number of pixels in synthesized image.
+  The energy function in patch could be wriiten as: E = |px-pz|^2+mu*|Dx-Dz|^2
+  We choose to express the gradient of pixel (xx,yy) as Dxx = pixel(x+1)-pixel(x-1) we could not perform as before as we have crossed gradient term, but the problem of gradient equals to zeros remains linear, so we solve linear problem Ax = b.  
+  When calculating the gradient matrix considering only the derivate in x direction, the contribution of one pixel in the gradient matrix A and demanded value b is(we choose mu = 0.1):
+  derivate / [x*heignt+y, x*height+y]
+  in A for [x*heignt+y, x*height+y]:2              b [x*height+y]: z(x,y)
+
+  derivate / [x*heignt+y+1, x*height+y+1]
+  in A for [x*height+y+1, x*height+y+1] : mu*2
+     A for [x*height+y+1, x*height+y-1] : mu*-2       b [x*height+y+1]: z(x+1,y)-z(x-1,y) 
+     A for [x*height+y-1, x*height+y-1] : mu*2
+     A for [x*height+y+1, x*height+y-1] : mu*-2       b [x*height+y-1]: z(x-1,y)-z(x+1,y)
+
+  The similar is for derivate / [x*height+y-1, x*]
+
+  The matrix A becomes 2 0 -2mu 
+                         2 0 -2mu
+                           2 0 -2mu etc.
+*/
+
 void solve_grad(const std::vector<cv::Point>& z, const std::vector<cv::Point>& x, const cv::Mat& ref_image, cv::Mat& image, int width)
 {
   printf("Optmize_grad\n");
@@ -319,10 +356,11 @@ void solve_one_channel_basic(const vector<Point>& z, const vector<Point>& x, con
 
   for(int k = 0; k < x.size(); ++k)
   {
+    // For every neighbour, modify all pixel coeffiecents         
     for(int irow = -width/2 + x[k].x; irow < width/2 + x[k].x; ++irow)
     {
       for(int icol = -width/2 + x[k].y; icol < width/2 + x[k].y; ++icol)
-      {
+      { 
         X[irow + icol*image_rows] += 1;
         Z[irow + icol*image_rows] += ref_image.at<unsigned char>(z[k].x+(irow - x[k].x), z[k].y+(icol - x[k].y));
       }
@@ -340,11 +378,20 @@ void solve_one_channel_basic(const vector<Point>& z, const vector<Point>& x, con
   }
 }
 
+/*
+  Solve energy optimization problem and render image, O(n), n the number of pixels in synthesized image.
+  As the energy is quadratic in patch, E = |px-pz|^2
+  By giving the gradient expression equal to zero, we obtain:
+    X[x,y] = (1/n)*[Z1+Z2+...+Zn], n the nearest neighbours of neighbour that contains pixels X[x,y]
+  We could see that the gradient do not have crosses terms, so we calculate the above sum directly which gives O(n) complexity.    
+*/
+
 void solve_basic(const std::vector<cv::Point>& z, const std::vector<cv::Point>& x, const cv::Mat& ref_image, cv::Mat& image, int width)
 {
   Mat im_one_channel(image.size(), CV_8UC1);
   Mat ref_one_channel(ref_image.size(), CV_8UC1);
 
+  // Solve the problem channel by channel
   for(int ichannel = 0; ichannel < 3; ++ichannel)
   {
     for(int irow = 0; irow < image.rows; ++irow)
@@ -462,6 +509,12 @@ void solve_one_channel_ggrad(const vector<Point>& z, const vector<Point>& x, con
     }
   }   
 }
+
+/*
+  The solve_ggrad function has the same structure as solve_grad.
+  It adds gaussian fall-off function above solve_grad.
+  For reference, see function: solve_grad, solve_gaussian 
+*/
 
 void solve_ggrad(const std::vector<cv::Point>& z, const std::vector<cv::Point>& x, const cv::Mat& ref_image, cv::Mat& image, int width)
 {
