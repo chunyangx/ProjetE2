4eaf8e96a9e49b1aa6a9a899a12b13b200fce6d6
Loic Fagot-Bouquet <loic@laptop.(none)>
2013-03-03 09:58:48 +0100

    Gaussian weights results and report.

--- Texture_synthesis.cpp
+++ Texture_synthesis.cpp
@@ -60,8 +60,9 @@ void main_loop(const Mat& texture_ref,Mat& texture,const int w, int& random_init
 
     //solve_opt_bis(z, x, texture_ref, texture, w);
     //wsolve_opt_bis(z, x, texture_ref, texture, w, weights);
-    solve_basic(z, x, texture_ref, texture, w);
+    //solve_basic(z, x, texture_ref, texture, w);
     //solve_ggrad(z, x, texture_ref, texture, w);
+    wsolve_gaussian(z, x, texture_ref, texture, w, weights);
     findTreeNNH(x, w, texture, texture_ref, root, z);
     update_weights(x, z, w, texture, texture_ref, weights);
 
